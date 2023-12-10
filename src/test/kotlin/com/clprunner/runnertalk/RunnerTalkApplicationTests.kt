9d760f30142ac54f60974295d99d58f2a3c5c9f6
박진휘 <best888810@naver.com>
2023-12-10 12:19:56 +0900

    초기 설정

---
+++ src/test/kotlin/com/clprunner/runnertalk/RunnerTalkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clprunner.runnertalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RunnerTalkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
